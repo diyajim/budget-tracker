9ff01021eb05243be8549279dca7e71b15d10a07
adamlzz <adamshenhav@gmail.com>
2026-05-03 21:18:49 -0700

    CRUD

--- server.js
+++ server.js
@@ -11,23 +11,44 @@ app.use(cors());
 app.use(express.json());
 
 // post request at /payment, gets amount and category from request body and inserts values. returns rows
-app.post("/payment", async (request, response) => {
-    const {paymentmethodid, methodname} = request.body;
+app.post("/paymentcreate", async (request, response) => {
+    const {methodname} = request.body;
 
     const result = await pool.query(
-        "INSERT INTO paymentmethod (paymentmethodid, methodname) VALUES ($1, $2) RETURNING *",
-        [paymentmethodid, methodname]
+        "INSERT INTO paymentmethod (methodname) VALUES ($1) RETURNING *",
+        [methodname]
     );
 
     response.json(result.rows);
 });
+
+app.get("/paymentread", async (request, response) => {
+    const result = await pool.query(
+        "SELECT * FROM paymentmethod ORDER BY paymentmethodid"
+    );
+
+    response.json(result.rows);
+})
+
+app.put("/paymentupdate", async (request, response) => {
+    const {paymentmethodid, methodname} = request.body;
+
+    const result = await pool.query(
+        "UPDATE paymentmethod SET methodname = $2 WHERE paymentmethodid = $1 RETURNING *",
+        [paymentmethodid, methodname]
+    );
+
+    response.json(result.rows);
+})
+
+
 // delete request at /paymentdelete, gets id from request body and deletes corresponding row. returns deleted row
 app.delete("/paymentdelete", async (request, response) => {
-    const {id} = request.body;
+    const {paymentmethodid} = request.body;
 
     const result = await pool.query(
         "DELETE FROM paymentmethod WHERE paymentmethodid = $1 RETURNING *",
-        [id]
+        [paymentmethodid]
     );
 
     response.json(result.rows);
